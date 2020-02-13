9c99ee23dea98e86e69302862d39ef681c4aced8
lizhe <lizhenhui1122@163.com>
2020-02-13 23:16:42 +0800

    Update for searching list company

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,62 @@
 <template>
     <div>
-        <h1>查询A股上市公司</h1>
-        <p>{{msg}}</p>
+        <div class="sub-title">输入后匹配输入建议</div>
+        <el-autocomplete class="inline-input" v-model="state1" :fetch-suggestions="querySearch" placeholder="请输入内容"
+                         :trigger-on-focus="false" @select="handleSelect"><el-button slot="append" icon="el-icon-search" @click = "getInputValue"></el-button></el-autocomplete>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     export default {
-        data () {
+        name: 'App',
+        data() {
             return {
-                msg: "请输入上市公司代码"
+                securities: [],
+                info:[],
+                state1: '',
             }
         },
-        name: "Search"
+        components: {},
+
+        methods: {
+            querySearch(queryString, cb) {
+                let securities = this.securities;
+                let results = queryString ? securities.filter(this.createFilter(queryString)) : securities;
+                // 调用 callback 返回建议列表的数据
+                cb(results);
+            },
+            createFilter(queryString) {
+                return (securities) => {
+                    return (securities.value.toLowerCase().indexOf(queryString.toLowerCase()) >= 0);
+                };
+            },
+            handle_securities_4_combobox(response) {
+                // this.securities = [{"value": response.data["hello world"][0][1] + "(" + response.data["hello world"][0][0] + ")"}];
+                for (let i in response.data["hello world"]) {
+                    this.securities.unshift({"value": response.data["hello world"][i][1] + "(" + response.data["hello world"][i][0] + "," + response.data["hello world"][i][2]+")"})
+                }
+                this.info = response.data["hello world"]
+            },
+            handleSelect(item) {
+                console.log(item);
+            },
+            getInputValue(){
+                let code = this.state1.replace(/[^0-9]/ig,"");
+                alert(code)
+            }
+            // jump() {
+            //     let code = this.state1.replace(/[^0-9]/ig,"");
+            //     window.location = "forcast.html?code="+code;
+            // }
+        },
+        mounted() {
+            axios
+                .get('http://127.0.0.1:8001/securities')
+                // .then(response => (this.securities = [{"value": response.data["hello world"][1][1]}, {"value": "3"}])); //[{"values": "1"}, {"values": "2"}])); //response.data["hello world"][1][1]));
+                .then(this.handle_securities_4_combobox);
+            // this.securities = this.loadAll();
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import router from "@/router";
+
+Vue.use(ElementUI);
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;       // block 生产环境中的Tip消息
 
